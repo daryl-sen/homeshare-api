f74887a7c0874217b80e570246d7973488cf59a8
Daryl <tsdaryl@gmail.com>
2022-07-29 20:22:11 -0700

    create app bar

---
+++ frontend/src/modules/layout/MainAppBar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MainDrawer from "./MainDrawer";
+
+export default function MainAppBar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => setIsDrawerOpen((prev) => !prev);
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+              HomeShare
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <MainDrawer isOpen={isDrawerOpen} setIsOpen={toggleDrawer} />
+    </>
+  );
+}
