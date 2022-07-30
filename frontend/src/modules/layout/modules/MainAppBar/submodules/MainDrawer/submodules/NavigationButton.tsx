04965bb192e30beaecea50347670f41174561afa
Daryl <tsdaryl@gmail.com>
2022-07-29 22:08:53 -0700

    create navigation button

---
+++ frontend/src/modules/layout/modules/MainAppBar/submodules/MainDrawer/submodules/NavigationButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+interface NavigationButtonProps {
+  children: string;
+  icon: JSX.Element;
+}
+
+export default function NavigationButton({
+  children,
+  icon,
+}: NavigationButtonProps) {
+  return (
+    <Button endIcon={icon} sx={{ justifyContent: "flex-end" }}>
+      {children}
+    </Button>
+  );
+}
