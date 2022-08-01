ec6d6023c2ab18a49a412b69f1f0f125e8c027cd
Daryl <tsdaryl@gmail.com>
2022-08-01 12:24:48 -0700

    backend render react app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 3200;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const rootPath = path.join(__dirname);
+
+app.use(express.static(rootPath));
+
+app.get("/api", (req, res) => {
+  res.send("Backend API coming soon!");
+});
+
+app.get("*", (req, res) => {
+  return res.sendFile("index.html", { root: rootPath });
 });
 
 app.listen(port, () => {
