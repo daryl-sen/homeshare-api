0918911da68c270f5eb9c21a0d9a1af42fc7547c
Daryl <tsdaryl@gmail.com>
2022-09-24 15:42:00 -0700

    conditionally render api docs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import bodyParser from "body-parser";
-// if routes don't exist, run ``
+// if routes don't exist, run `yarn run dev` first, refer to readme
 import { RegisterRoutes } from "../build/routes";
 import express, { Response as ExResponse, Request as ExRequest } from "express";
 import swaggerUi from "swagger-ui-express";
+require("dotenv").config();
 
 export const app = express();
 
@@ -14,6 +15,9 @@ app.use(
 );
 
 app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  if (process.env.APP_ENV === "production") {
+    return res.send("Docs not available on production");
+  }
   return res.send(
     swaggerUi.generateHTML(await import("../build/swagger.json"))
   );
