bfb421c169e52760af9d11b0211610fc07def6f6
Daryl <tsdaryl@gmail.com>
2022-09-26 19:59:39 -0700

    update UPDATE query to be dynamically built

--- src/db/queries/userQueries.ts
+++ src/db/queries/userQueries.ts
@@ -8,22 +8,15 @@ const USER_QUERIES = {
       last_login VARCHAR(255) NOT NULL,
       is_admin BOOLEAN NOT NULL
     );`,
-    DROP_TABLE: `DROP TABLE IF EXISTS users`,
+    DROP_TABLE: `DROP TABLE IF EXISTS users;`,
   },
   CREATE_USER: `INSERT INTO users (user_name, display_name, encrypted_password, is_admin, last_login) VALUES (?, ?, ?, ?, ?);`,
-  READ_USER: `SELECT * FROM users
-  WHERE users.user_name=?`,
-  UPDATE_USER: `UPDATE users
-  SET (
-    user_name=?,
-    display_name=?,
-    encrypted_password=?,
-    is_admin=?,
-    last_login=?
-  )
-  WHERE users.id=?`,
+  READ_USER: `SELECT * FROM users WHERE users.user_name=?;`,
+  READ_USER_BY_ID: `SELECT * FROM users WHERE users.id=?;`,
+  // the middle part of this query will be built dynamically in the service
+  UPDATE_USER: { START: `UPDATE users SET `, END: ` WHERE users.id=?;` },
   DELETE_USER: `DELETE FROM users
-  WHERE users.id=?`,
+  WHERE id=?;`,
 };
 
 export default USER_QUERIES;
