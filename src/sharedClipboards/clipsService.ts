460028ea7708c6c42e36962dee9cb244fc396f37
Daryl <tsdaryl@gmail.com>
2022-10-06 14:00:00 -0700

    create clip creation method

--- src/sharedClipboards/clipsService.ts
+++ src/sharedClipboards/clipsService.ts
@@ -3,11 +3,11 @@
 import { BaseService } from '../common/baseService';
 import CLIP_QUERIES from '../db/queries/clipQueries';
 import camelize from '../helpers/camelize';
-import { Clip } from './clip';
+import { Clip, ClipCreationResponse } from './clip';
 
-export type CipCreationParams = Pick<
+export type ClipCreationParams = Pick<
   Clip,
-  "clipName" | "encryptedContent" | "userId" | "createdAt" | "modifiedAt"
+  "clipName" | "encryptedContent" | "userId"
 >;
 
 export class ClipsService extends BaseService {
@@ -15,13 +15,35 @@ export class ClipsService extends BaseService {
     super();
   }
 
-  public async getByUserId(userId: number): Promise<Clip[]> {
+  public async get(userId: number): Promise<Clip[]> {
     return await this.getClipsQuery(userId);
+  }
+
+  public async create(
+    creationParams: ClipCreationParams
+  ): Promise<ClipCreationResponse> {
+    return await this.createClipQuery(creationParams);
   }
 
   private async getClipsQuery(userId: number): Promise<Clip[]> {
     return camelize(
       await this.runQueryAndReturn(CLIP_QUERIES.READ_CLIPS_BY_USER_ID, [userId])
     ) as Clip[];
+  }
+
+  private async createClipQuery(
+    creationParams: ClipCreationParams
+  ): Promise<ClipCreationResponse> {
+    const { clipName, encryptedContent, userId } = creationParams;
+    const createdAt = new Date().toISOString();
+    const modifiedAt = createdAt;
+
+    return (await this.runQueryAndReturn(CLIP_QUERIES.CREATE_CLIP, [
+      userId,
+      clipName,
+      encryptedContent,
+      modifiedAt,
+      createdAt,
+    ])) as ClipCreationResponse;
   }
 }
